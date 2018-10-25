72afde91ce547ab913796e3fdd2c84d2a9d1720d
Kirill Agalakov <agalakov@devexperts.com>
2018-10-25 15:56:22 +0300

    update

--- index.ts
+++ index.ts
@@ -2,41 +2,19 @@
 
 import * as path from 'path';
 import * as fs from 'fs-extra';
-import { Spec, Schema, Path } from 'swagger-schema-official';
-import { tryCatch } from 'fp-ts/lib/Either';
-import { fromEither, fromRefinement, fromNullable, option, Option } from 'fp-ts/lib/Option';
-import { array } from 'fp-ts/lib/Array';
-import { tuple } from 'fp-ts/lib/function';
+import { fromNullable } from 'fp-ts/lib/Option';
 import * as prettier from 'prettier';
-import { sequenceT } from 'fp-ts/lib/Apply';
-import {
-	SwaggerObject,
-	InfoObject,
-	SecurityDefinitionsObject,
-	SecurityRequirementObject,
-	PathsObject,
-	stringOption,
-	ResponsesDefinitionsObject,
-} from './swagger';
-import { PathReporter } from 'io-ts/lib/PathReporter';
-import {
-	PathItemObject,
-	OperationObject,
-	stringArrayOption,
-	ParameterObject,
-	ResponseObject,
-	SchemaObject,
-} from './swagger';
+
+import { SwaggerObject } from './swagger';
 import { map, write } from './fs';
 import { serializeSwaggerObject } from './serializer2';
 
 const root = process.cwd();
 const destination = path.resolve('./out');
 const name = 'spec';
-const json = path.resolve(root, './ads-swagger-specs.json');
-const prettierConfig = path.resolve(root, '.prettierrc');
+const json = path.resolve(root, './specs/ads-swagger-specs.json');
 
-// fs.mkdirSync(path.resolve(destination));
+const prettierConfig = path.resolve(root, '.prettierrc');
 
 async function run() {
 	const raw = await fs.readFile(json);
@@ -44,9 +22,9 @@ async function run() {
 	const decoded = SwaggerObject.decode(spec);
 	const serialized = decoded.map(spec => serializeSwaggerObject(name, spec));
 	const config = fromNullable(await prettier.resolveConfig(prettierConfig));
+	await fs.remove(destination);
 	if (serialized.isRight() && config.isSome()) {
 		const formatted = map(serialized.value, serialized => prettier.format(serialized, config.value));
-		await fs.remove(destination);
 		await fs.mkdirp(destination);
 		await write(destination, formatted);
 	}
