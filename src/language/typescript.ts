998debb2388f7bf6557490f3b041cb24dc018e42
Kirill Agalakov <agalakov@devexperts.com>
2018-11-07 18:28:33 +0300

    fix: fix unknown body parameter

--- src/language/typescript.ts
+++ src/language/typescript.ts
@@ -57,13 +57,6 @@ const serializedType = (type: string, io: string, dependencies: TDepdendency[], 
 	refs,
 });
 
-const SERIALIZED_MIXED_DICTIONARY_TYPE = serializedType(
-	'Record<string, t.mixed>',
-	't.Dictionary',
-	EMPTY_DEPENDENCIES,
-	EMPTY_REFS,
-);
-
 type TSerializedParameter = TSerializedType & {
 	isRequired: boolean;
 };
@@ -138,15 +131,19 @@ const CONTROLLERS_DIRECTORY = 'controllers';
 const DEFINITIONS_DIRECTORY = 'definitions';
 const CLIENT_DIRECTORY = 'client';
 const CLIENT_FILENAME = 'client';
+const UTILS_DIRECTORY = 'utils';
+const UTILS_FILENAME = 'utils';
 
 const getRelativeRoot = (cwd: string) => path.relative(cwd, ROOT_DIRECTORY);
 const getRelativeDefinitionPath = (cwd: string, definitionFileName: string): string =>
 	`${getRelativeRoot(cwd)}/${DEFINITIONS_DIRECTORY}/${definitionFileName}`;
 const getRelativeClientPath = (cwd: string): string => `${getRelativeRoot(cwd)}/${CLIENT_DIRECTORY}/${CLIENT_FILENAME}`;
+const getRelativeUtilsPath = (cwd: string): string => `${getRelativeRoot(cwd)}/${UTILS_DIRECTORY}/${UTILS_FILENAME}`;
 
 export const serialize: TSerializer = (name: string, swaggerObject: TSwaggerObject): TDirectory =>
 	directory(name, [
 		directory(CLIENT_DIRECTORY, [file(`${CLIENT_FILENAME}.ts`, client)]),
+		directory(UTILS_DIRECTORY, [file(`${UTILS_FILENAME}.ts`, utils)]),
 		...catOptions([swaggerObject.definitions.map(serializeDefinitions)]),
 		serializePaths(swaggerObject.paths),
 	]);
@@ -295,7 +292,14 @@ const serializeSchemaObject = (schema: TSchemaObject, rootName: string, cwd: str
 						return toObjectType(serialized, serialized.refs.includes(rootName) ? some(rootName) : none);
 					}),
 				)
-				.getOrElse(SERIALIZED_MIXED_DICTIONARY_TYPE);
+				.getOrElseL(() =>
+					serializedType(
+						'unknown',
+						'unknownType',
+						[dependency('unknownType', getRelativeUtilsPath(cwd))],
+						EMPTY_REFS,
+					),
+				);
 		}
 	}
 };
@@ -584,7 +588,7 @@ const client = `
 	export type TAPIRequest = {
 		url: string;
 		query?: object;
-		body?: object;
+		body?: unknown;
 	};
 
 	export type TFullAPIRequest = TAPIRequest & {
@@ -605,6 +609,17 @@ const client = `
 			Object.setPrototypeOf(this, ResponseValiationError);
 		}
 	}
+`;
+
+const utils = `
+	import * as t from 'io-ts';
+	export const unknownType = new class UnknownType extends t.Type<unknown> {
+		readonly _tag: 'UnknownType' = 'UnknownType';
+	
+		constructor() {
+			super('unknownType', (_: unknown): _ is unknown => true, t.success, t.identity);
+		}
+	}();
 `;
 
 const hasRequiredParameters = (parameters: Array<TQueryParameterObject | TBodyParameterObject>): boolean =>
