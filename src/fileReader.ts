5564d7f44abb932dbff4449b23534d1c6fe1c71b
Anton Koshkin <akoshkin@devexperts.com>
2018-12-07 13:53:35 +0300

    feat: add yaml support
    Closes #7

--- src/fileReader.ts
+++ src/fileReader.ts
@@ -1,3 +1,6 @@
 import { TFileReader } from './index';
+import * as yaml from 'js-yaml';
 
 export const fromJSON: TFileReader = buffer => JSON.parse(buffer.toString());
+
+export const fromYaml: TFileReader = buffer => yaml.safeLoad(buffer.toString());
--- test/index.ts
+++ test/index.ts
@@ -3,15 +3,25 @@
 import * as path from 'path';
 import { generate } from '../src';
 import { serialize } from '../src/language/typescript';
-import { fromJSON } from '../src/fileReader';
+import { fromJSON, fromYaml } from '../src/fileReader';
 
 const self = path.resolve(__dirname);
 
 generate({
-	pathsToSpec: [path.resolve(self, './specs/swagger.json'), path.resolve(self, './specs/common.json')],
-	out: path.resolve(self, './out'),
+	pathsToSpec: [path.resolve(self, './specs/json/swagger.json'), path.resolve(self, './specs/json/common.json')],
+	out: path.resolve(self, './out/json'),
 	serialize,
 	fileReader: fromJSON,
+}).catch(error => {
+	console.error(error);
+	process.exit(1);
+});
+
+generate({
+	pathsToSpec: [path.resolve(self, './specs/yaml/swagger.yml'), path.resolve(self, './specs/yaml/common.yml')],
+	out: path.resolve(self, './out/yaml'),
+	serialize,
+	fileReader: fromYaml,
 }).catch(error => {
 	console.error(error);
 	process.exit(1);
