ca7c1649712ca521c23c9483fa8820c70269e928
Anton Koshkin <15276485+scink@users.noreply.github.com>
2018-12-13 19:16:00 +0300

    feat: add multiple file support
    Closes #2

--- src/fileReader.ts
+++ src/fileReader.ts
@@ -1,3 +1,9 @@
-import { TFileReader } from './index';
+import * as yaml from 'js-yaml';
+
+export type TJSON = {
+	[key: string]: unknown;
+};
+export type TFileReader = (buffer: Buffer) => TJSON;
 
 export const fromJSON: TFileReader = buffer => JSON.parse(buffer.toString());
+export const fromYaml: TFileReader = buffer => yaml.safeLoad(buffer.toString());
