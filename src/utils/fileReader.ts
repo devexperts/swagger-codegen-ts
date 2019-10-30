863e5544fb9273ccab1d7fff950b7aabbb4ee84b
raveclassic <raveclassic@gmail.com>
2019-10-30 12:30:13 +0300

    build(deps): cleanup

--- src/utils/fileReader.ts
+++
@@ -1,7 +0,0 @@
-import * as yaml from 'js-yaml';
-
-export interface JSON extends Record<string, unknown> {}
-export type FileReader = (buffer: Buffer) => JSON;
-
-export const fromJSON: FileReader = buffer => JSON.parse(buffer.toString());
-export const fromYaml: FileReader = buffer => yaml.safeLoad(buffer.toString());
