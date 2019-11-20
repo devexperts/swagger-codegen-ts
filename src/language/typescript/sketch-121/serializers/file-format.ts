47c1422ba38a164b60c7508bc4dc30784497cf49
raveclassic <raveclassic@gmail.com>
2019-11-20 16:52:26 +0300

    chore: split document.ts to different files

--- src/language/typescript/sketch-121/serializers/file-format.ts
+++ src/language/typescript/sketch-121/serializers/file-format.ts
@@ -2,19 +2,16 @@ import { combineReader } from '@devexperts/utils/dist/adt/reader.utils';
 import { serializeDocument } from './document';
 import { FileFormat } from '../../../../schema/sketch-121/file-format';
 import { Either } from 'fp-ts/lib/Either';
-import { file, fragment, FSEntity } from '../../../../utils/fs';
+import { fragment, FSEntity } from '../../../../utils/fs';
 import { pipe } from 'fp-ts/lib/pipeable';
-import { array, either, nonEmptyArray, option } from 'fp-ts';
+import { array, nonEmptyArray, option } from 'fp-ts';
 import { combineEither } from '@devexperts/utils/dist/adt/either.utils';
 import { Option } from 'fp-ts/lib/Option';
 
 export const serializeFileFormat = combineReader(
 	serializeDocument,
 	serializeDocument => (fileFormat: FileFormat): Either<Error, Option<FSEntity>> => {
-		const document = pipe(
-			serializeDocument(fileFormat.document),
-			either.map(option.map(document => file('document.ts', document))),
-		);
+		const document = serializeDocument(fileFormat.document);
 
 		return combineEither(document, document =>
 			pipe(
