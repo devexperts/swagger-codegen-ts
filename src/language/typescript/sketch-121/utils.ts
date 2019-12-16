6ba089cf513bead00cb5d76c9644d92e8a6a0034
ipavelleds <ipavelleds@gmail.com>
2019-12-16 09:09:43 +0000

    fix(sketch): avoid collision for names with counter

--- src/language/typescript/sketch-121/utils.ts
+++ src/language/typescript/sketch-121/utils.ts
@@ -10,6 +10,13 @@ export const createNameStorage = () => {
 	const uuidToName = new Map<string, string>();
 	const nameToCounter = new Map<string, number>();
 
+	const getSafeNameWithCounter = (safeName: string): string => {
+		const counter = nameToCounter.get(safeName) || 1;
+		const nameWithCounter = safeName + counter;
+		nameToCounter.set(safeName, counter + 1);
+		return !nameToUuid.has(nameWithCounter) ? nameWithCounter : getSafeNameWithCounter(safeName);
+	}
+
 	const getSafeName = (uuid: string, name: string): string => {
 		const safeName = getSafePropertyName(name);
 		// check if we have a name for such uuid
@@ -27,11 +34,9 @@ export const createNameStorage = () => {
 			return safeName;
 		}
 		// we already have such safeName stored - increase counter and store under uuid
-		const counter = nameToCounter.get(safeName) || 1;
-		const nameWithCounter = safeName + counter;
+		const nameWithCounter = getSafeNameWithCounter(safeName);
 		nameToUuid.set(nameWithCounter, uuid);
 		uuidToName.set(uuid, nameWithCounter);
-		nameToCounter.set(safeName, counter + 1);
 		return nameWithCounter;
 	};
 
