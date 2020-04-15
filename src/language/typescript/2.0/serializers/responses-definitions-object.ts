dfb2b36c987b74f07064ca44728d38554c2f43ee
Dmitry Kokovtsev <kokovtsev@gmail.com>
2020-04-15 12:51:39 +0300

    fix: void response missing from union (#104) (#110)
    BREAKING CHANGE: void response is added to resulting response type

--- src/language/typescript/2.0/serializers/responses-definitions-object.ts
+++ src/language/typescript/2.0/serializers/responses-definitions-object.ts
@@ -1,14 +1,13 @@
 import { getIOName, getTypeName } from '../../common/utils';
 import { addPathParts, Ref } from '../../../../utils/ref';
 import { ResponsesDefinitionsObject } from '../../../../schema/2.0/responses-definitions-object';
-import { Either, right } from 'fp-ts/lib/Either';
+import { Either } from 'fp-ts/lib/Either';
 import { directory, file, FSEntity } from '../../../../utils/fs';
 import { pipe } from 'fp-ts/lib/pipeable';
-import { either, option, record } from 'fp-ts';
+import { either, record } from 'fp-ts';
 import { sequenceEither } from '@devexperts/utils/dist/adt/either.utils';
 import { ResponseObject } from '../../../../schema/2.0/response-object';
 import { serializeResponseObject } from './response-object';
-import { SERIALIZED_VOID_TYPE } from '../../common/data/serialized-type';
 import { serializeDependencies } from '../../common/data/serialized-dependency';
 
 export const serializeResponsesDefinitionsObject = (
@@ -31,7 +30,6 @@ export const serializeResponsesDefinitionsObject = (
 const serializeResponse = (from: Ref, responseObject: ResponseObject): Either<Error, FSEntity> =>
 	pipe(
 		serializeResponseObject(from, responseObject),
-		option.getOrElse(() => right(SERIALIZED_VOID_TYPE)),
 		either.map(serialized => {
 			const dependencies = serializeDependencies(serialized.dependencies);
 			return file(
